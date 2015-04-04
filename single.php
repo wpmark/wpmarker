119e2360fe92ce95f6a07013456546217274ed22
Mark Wilkinson <mark@markwilkinson.me>
2015-04-04 20:04:09 +0100

    Add get header / footer to single and page.php
    Makes the front end load correctly.

--- single.php
+++ single.php
@@ -2,3 +2,11 @@
 /* template: single.php
  * this template is loaded for all the sites permalink post pages
  */
+
+ /* get the header template */
+ get_header();
+ 
+ 
+ 
+ /* get the footer template */
+ get_footer();
