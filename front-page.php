dfb7a0f17c9f75d2a66b825343d4434a91c9639b
Mark Wilkinson <mark@markwilkinson.me>
2015-04-05 13:37:30 +0100

    Form Styles

--- front-page.php
+++ front-page.php
@@ -7,7 +7,28 @@
  /* get the header template */
  get_header();
  
- 
+ 	?>
+ 	<div class="container">
+	 	
+	 	<form>
+		 	
+		 	<div class="select-wrapper">
+			 	<select name="something">
+				 	<option value="1">Something</option>
+				 	<option value="2">Something Else</option>
+			 	</select>
+			 	<span class="select-arrow dashicons dashicons-arrow-down-alt2"></span>
+		 	</div>
+		 	
+		 	<input type="text" placeholder="Placeholder" value="ffff" />
+		 	
+		 	<textarea name="textarea"></textarea>
+		 	
+	 	</form>
+	 	
+ 	</div>
+ 	
+ 	<?php
  
  /* get the footer template */
  get_footer();
