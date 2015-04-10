a49ff7d3fefc3c966f3afa8eaea6c241094e23e7
Mark Wilkinson <mark@markwilkinson.me>
2015-04-10 16:39:00 +0100

    Markup for Content.php

--- parts/loops/content.php
+++ parts/loops/content.php
@@ -1,5 +1,21 @@
 <article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
 	
+	<header class="entry-header">
+		
+		<h1 class="entry-title"><?php the_title(); ?></h1>
+		
+	</header>
 	
+	<div class="entry-content">
+		
+		
+		
+	</div><!-- // entry-content -->
+	
+	<footer class="entry-footer">
+		
+		
+		
+	</footer><!-- // entry-footer -->
 	
 </article>
