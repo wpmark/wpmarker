b9649801ddd1ab37ec52151c8ff00abd3292b3e3
Mark Wilkinson <mark@markwilkinson.me>
2015-06-18 16:41:25 +0100

    New Gridset
    This was done to give a better layout engine with the wider desktop layout for larger screens.
    
    At the same time the conditional gridset styles are loaded for older versions of IE - notably IE8 and IE9.

--- front-page.php
+++ front-page.php
@@ -4,10 +4,46 @@
  * best used in conjunction with setting static page for your home page in settings > reading
  */
 
- /* get the header template */
- get_header();
- 
- 	
- 	 
- /* get the footer template */
- get_footer();
+/* get the header template */
+get_header();
+
+	?>
+	
+	<div class="container">
+	 	
+	 	<div style="border: 1px solid #333333; padding: 16px">
+		 	
+		 	<div class="ms-hide m-hide t-hide d-all">
+			 	
+			 	<h1>Large Desktops</h1>
+			 	
+		 	</div>
+		 	
+		 	<div class="ms-hide m-hide t-all d-hide">
+			 	
+			 	<h1>Smaller Desktops</h1>
+			 	
+		 	</div>
+		 	
+		 	<div class="ms-hide m-all t-hide d-hide">
+			 	
+			 	<h1>Tablets</h1>
+			 	
+		 	</div>
+		 	
+		 	<div class="ms-all m-hide t-hide d-hide">
+			 	
+			 	<h1>Phones</h1>
+			 	
+		 	</div>
+		 	
+		 	<div style="clear: both;"></div>
+		 	
+	 	</div>
+	 	
+ 	</div>
+	
+	<?php
+
+/* get the footer template */
+get_footer();
--- inc/scripts.php
+++ inc/scripts.php
@@ -5,6 +5,8 @@
  */
 function wpmarker_enqueue_scripts_styles() {
 	
+	global $wp_styles;
+	
 	/* load the theme js file */
 	wp_enqueue_script(
 		'wpmarker_js',
@@ -19,10 +21,33 @@ function wpmarker_enqueue_scripts_styles() {
 	wp_enqueue_style(
 		'wpmarker_styles',
 		get_template_directory_uri(). '/assets/css/style.css',
-		array( 'dashicons' ), // no dependencies
+		array( 'dashicons' ), // load dashicons before this
 		array(), // no version
 		'all' // for all media types
 	);
+	
+	/* load the themes styles */
+	wp_enqueue_style(
+		'wpmarker_gridset',
+		get_template_directory_uri(). '/assets/css/gridset.css',
+		array( 'wpmarker_styles' ), // load theme styles first
+		array(), // no version
+		'all' // for all media types
+	);
+	
+	/**
+     * Load our IE 9 specific stylesheet:
+     * <!--[if IE 9]> ... <![endif]-->
+     */
+	wp_enqueue_style( 'wpmarker_ie9_style', get_template_directory_uri(). '/assets/css/gridset-ie-9.css', array( 'wpmarker_styles' ) );
+	$wp_styles->add_data( 'wpmarker_ie9_style', 'conditional', 'IE 9' );
+	
+	/**
+     * Load our IE 8 specific stylesheet:
+     * <!--[if lte IE 8]> ... <![endif]-->
+     */
+	wp_enqueue_style( 'wpmarker_ie8_style', get_template_directory_uri(). '/assets/css/gridset-ie-lte8.css', array( 'wpmarker_styles' ) );
+	$wp_styles->add_data( 'wpmarker_ie8_style', 'conditional', 'lte IE 8' );
 	
 	/* add custom fonts */
 	wp_enqueue_style(
