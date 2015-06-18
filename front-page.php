aec324f77e296f0a01f032a95ad8597094ad8744
Mark Wilkinson <mark@markwilkinson.me>
2015-06-18 16:48:09 +0100

    Move Layout Test to Part
    Instead of directly in the front-page.php template it can be placed anywhere.

--- front-page.php
+++ front-page.php
@@ -7,43 +7,8 @@
 /* get the header template */
 get_header();
 
-	?>
+	/* get the layout test template part */
+	get_template_part( 'parts/layouttest' );
 	
-	<div class="container">
-	 	
-	 	<div style="border: 1px solid #333333; padding: 16px">
-		 	
-		 	<div class="ms-hide m-hide t-hide d-all">
-			 	
-			 	<h1>Large Desktops</h1>
-			 	
-		 	</div>
-		 	
-		 	<div class="ms-hide m-hide t-all d-hide">
-			 	
-			 	<h1>Smaller Desktops</h1>
-			 	
-		 	</div>
-		 	
-		 	<div class="ms-hide m-all t-hide d-hide">
-			 	
-			 	<h1>Tablets</h1>
-			 	
-		 	</div>
-		 	
-		 	<div class="ms-all m-hide t-hide d-hide">
-			 	
-			 	<h1>Phones</h1>
-			 	
-		 	</div>
-		 	
-		 	<div style="clear: both;"></div>
-		 	
-	 	</div>
-	 	
- 	</div>
-	
-	<?php
-
 /* get the footer template */
 get_footer();
